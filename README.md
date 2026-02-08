63fbcabf78484f2eac8a31bac360b48475621f2a
Jackson <rjtz822@gmail.com>
2026-02-08 01:01:47 -0700

    added theme and storage permissions for future testing, made other small changes, and updated readme with current progress

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Sinister-Six-Chrome-Extension
 This is our awesome group 6 repository for our CS 3250 assignments! We can update the name and description once we settle in.
 
-This is my edit to test pull requests.
+Current progress:
+management permission allows us to see currently installed themes!
+
+Current roadblocks:
+Only active themes are left installed.... Not sure how we'll get around this. Maybe we can use storage permissions to store themes in the extension and switch them from there
--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("myButton");
   const title = document.getElementById("title");
+  const text = document.getElementById("text2");
 
   if (!button || !title) {
     console.error("Element not found");
@@ -11,10 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const extensions = await chrome.management.getAll();
 
     // Example: find the first enabled extension
-    const enabledExtension = extensions.find(ext => ext.type === "theme" && ext.enabled);
+    const enabledExtension = extensions.find(
+      (ext) => ext.type === "theme" && ext.enabled,
+    );
+
+    const disabledExtension = extensions.find(
+      (ext) => ext.type === "theme" && ext.disabled,
+    );
 
     title.textContent = enabledExtension
       ? enabledExtension.name
-      : "No enabled extensions found";
+      : "No enabled themes found";
+
+    text2.textContent = enabledExtension
+      ? enabledExtension.version
+      : "No disabled themes found";
+
+    chrome.theme.update(
+      {
+        colors: {
+          frame: [255, 0, 0], // RGB for red
+          toolbar: [255, 200, 200], // optional
+          tab_text: [255, 255, 255],
+        },
+        images: {}, // optional
+        tints: {}, // optional
+        properties: {}, // optional
+      },
+      () => {
+        console.log("Theme applied!");
+      },
+    );
   });
 });
