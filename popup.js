0da2840d3044fcdc89e8bff9c0c8809e158c0abc
Jackson <rjtz822@gmail.com>
2026-02-16 12:40:17 -0700

    popup.js added for all logic. Updated popup.hmtl to support the new logic

--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("myButton");
-  const button2 = document.getElementById("button2");
+  const button2 = document.getElementById("link");
   const title = document.getElementById("title");
   const text = document.getElementById("text2");
+
+  const themeLink =
+    "https://chromewebstore.google.com/detail/pikmin-theme/bpmkhflicgoklmheccmojdbipcbmhcjg?utm_source=ext_app_menu";
 
   if (!button || !title) {
     console.error("Element not found");
@@ -11,10 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   button.addEventListener("click", async () => {
     const extensions = await chrome.management.getAll();
+
     var disabledExtension = null;
     for (let i = 0; i < extensions.length; i++) {
       if (extensions[i].type === "theme" && !extensions[i].enabled) {
         disabledExtension = extensions[i];
+      } else if (extensions[i].type === "theme") {
+        console.log(extensions[i].name);
       }
     }
 
@@ -42,14 +48,29 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
       disabledExtension = null;
+      for (let i = 0; i < extensions.length; i++) {
+        if (extensions[i].type === "theme" && !extensions[i].enabled) {
+          disabledExtension2 = extensions[0];
+          console.log(extensions[0]);
+        }
+      }
     }
 
     title.textContent = enabledExtension
       ? enabledExtension.name
       : "No enabled themes found";
 
-    text.textContent = enabledExtension
+    text.textContent = disabledExtension
       ? disabledExtension.name
       : "No disabled themes found";
+  });
+  button2.addEventListener("click", async () => {
+    window.open(themeLink, "_blank", "noopener");
+
+    for (let i = 0; i < extensions.length; i++) {
+      if (extensions[i].type === "theme" && !extensions[i].enabled) {
+        disabledExtension = extensions[i];
+      }
+    }
   });
 });
