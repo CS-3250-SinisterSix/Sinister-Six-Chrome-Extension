bc31116c26eae53adfd64632b82789edf305c717
Tilly15 <jttilly14@gmail.com>
2026-02-21 12:21:10 -0700

    Fix ESLint and stop tracking node_modules

---
+++ popup.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("myButton");
+  const button2 = document.getElementById("link");
+  const title = document.getElementById("title");
+
+  const themeLink =
+    "https://chromewebstore.google.com/detail/pikmin-theme/bpmkhflicgoklmheccmojdbipcbmhcjg?utm_source=ext_app_menu";
+
+  if (!button || !title) {
+    console.error("Element not found");
+    return;
+  }
+
+  button.addEventListener("click", async () => {
+    const extensions = await chrome.management.getAll();
+
+    let disabledExtension = null;
+    for (let i = 0; i < extensions.length; i++) {
+      if (extensions[i].type === "theme" && !extensions[i].enabled) {
+        disabledExtension = extensions[i];
+      } else if (extensions[i].type === "theme") {
+        console.log(extensions[i].name);
+      }
+    }
+
+    let enabledExtension = extensions.find(
+      (ext) => ext.type === "theme" && ext.enabled,
+    );
+
+    if (disabledExtension == null) {
+      //disable theme if there is currently a theme enabled
+      chrome.management.setEnabled(enabledExtension.id, false, () => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        } else {
+          console.log("Extension disabled!");
+        }
+      });
+    } else {
+      //enable theme if the installed theme is inactive
+      enabledExtension = disabledExtension;
+      chrome.management.setEnabled(disabledExtension.id, true, () => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        } else {
+          console.log("Extension enabled!");
+        }
+      });
+    }
+
+
+    title.textContent = enabledExtension 
+      ? enabledExtension.name
+      : "No enabled themes found";
+  });
+  button2.addEventListener("click", async () => {
+    window.open(themeLink, "_blank", "noopener");
+  });
+});
