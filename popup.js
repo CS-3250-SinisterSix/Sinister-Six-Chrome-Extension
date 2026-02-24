78bd2c6ce773093487a50f38410fd57974baddba
Jackson <rjtz822@gmail.com>
2026-02-24 14:34:42 -0700

    Updated Popup.js and popup.html to support link collection and navigation. Will move new functions to themes.js next!

--- popup.js
+++ popup.js
@@ -1,32 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const button = document.getElementById("myButton");
-  const button2 = document.getElementById("link");
+  const link = document.getElementById("link");
+  const clearButton = document.getElementById("clear");
+  const add = document.getElementById("add");
   const title = document.getElementById("title");
+  const text = document.getElementById("text2");
 
-  const themeLink =
-    "https://chromewebstore.google.com/detail/pikmin-theme/bpmkhflicgoklmheccmojdbipcbmhcjg?utm_source=ext_app_menu";
+  const makeLink = (name, ID) => {
+    const formattedName = name.replaceAll(" ", "-");
+    return (
+      "https://chromewebstore.google.com/detail/" + formattedName + "/" + ID
+    );
+  };
 
   if (!button || !title) {
     console.error("Element not found");
     return;
   }
 
-  button.addEventListener("click", async () => {
-    const extensions = await chrome.management.getAll();
+  const extensions = await chrome.management.getAll();
 
-    let disabledExtension = null;
-    for (let i = 0; i < extensions.length; i++) {
-      if (extensions[i].type === "theme" && !extensions[i].enabled) {
-        disabledExtension = extensions[i];
-      } else if (extensions[i].type === "theme") {
-        console.log(extensions[i].name);
-      }
+  let disabledExtension = null;
+  for (let i = 0; i < extensions.length; i++) {
+    if (extensions[i].type === "theme" && !extensions[i].enabled) {
+      disabledExtension = extensions[i];
+    } else if (extensions[i].type === "theme") {
+      console.log(extensions[i].name);
     }
+  }
 
-    const enabledExtension = extensions.find(
-      (ext) => ext.type === "theme" && ext.enabled,
-    );
+  const enabledExtension = extensions.find(
+    (ext) => ext.type === "theme" && ext.enabled,
+  );
 
+  const result = await chrome.storage.local.get(["links"]);
+  const themeLinks = result.links || [];
+
+  console.log("Total links:", themeLinks.length);
+
+  button.addEventListener("click", async () => {
     // We'll decide what to show in the UI without "nulling out" vars
     let themeToDisplay;
 
@@ -42,16 +54,19 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       }
       themeToDisplay = null;
+      disabledExtension = enabledExtension;
     } else {
       // enable theme if the installed theme is inactive
       chrome.management.setEnabled(disabledExtension.id, true, () => {
         if (chrome.runtime.lastError) {
           console.error(chrome.runtime.lastError);
         } else {
-          console.log("Extension enabled!");
+          console.log(disabledExtension);
         }
       });
+
       themeToDisplay = disabledExtension;
+      disabledExtension = null;
     }
 
     title.textContent = themeToDisplay
@@ -59,7 +74,20 @@ document.addEventListener("DOMContentLoaded", () => {
       : "No enabled themes found";
   });
 
-  button2.addEventListener("click", () => {
-    window.open(themeLink, "_blank", "noopener");
+  link.addEventListener("click", () => {
+    window.open(themeLinks[0], "_blank", "noopener");
+  });
+  add.addEventListener("click", async () => {
+    text.textContent = makeLink(enabledExtension.name, enabledExtension.id);
+    const newLink = makeLink(enabledExtension.name, enabledExtension.id);
+
+    if (!themeLinks.includes(newLink)) {
+      themeLinks.push(newLink);
+      await chrome.storage.local.set({ links: themeLinks });
+    }
+  });
+  clearButton.addEventListener("click", async () => {
+    await chrome.storage.local.clear();
+    console.log("Storage cleared");
   });
 });
